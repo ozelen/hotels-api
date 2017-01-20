c0b75f3ff7c6192a2cc8a724985b963616520d20
Oleksiy Zelenyuk <oleksiy.zelenyuk@symphony-solutions.eu>
2017-01-20 11:58:38 +0200

    WIP add authorization

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const SwaggerExpress = require('swagger-express-mw');
 const app = require('express')();
 const cors = require('cors');
 const connectMongo = require('./mongo');
+const jwt = require('express-jwt');
+
 module.exports = app; // for testing
 
 const {PORT, MONGO_HOST, MONGO_PORT, MONGO_DB} = process.env;
@@ -15,6 +17,17 @@ connectMongo({
   host : MONGO_HOST || '0.0.0.0',
   port : MONGO_PORT || 32768,
   db   : MONGO_DB   || 'hotels'
+});
+
+app.use(jwt({secret: 'ski123'}).
+  unless({path:[
+    {url: '/hotels', methods:['GET', 'OPTIONS']},
+    {url: /^\/hotels\/.*/, methods:['GET', 'OPTIONS']}
+  ]}));
+
+app.use((err, req, res, next) => {
+  err && err.name === 'UnauthorizedError' &&
+    res.status(401).send('invalid token...');
 });
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
